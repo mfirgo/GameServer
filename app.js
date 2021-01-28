f7b266c9dc249f0c51c51f91659486aeea5b0e4d
wiklam <wiktorpilar99@gmail.com>
2021-01-28 16:52:25 +0100

    Move functions handling rooms to room.js

--- app.js
+++ app.js
@@ -10,41 +10,9 @@ app.get('/', (req, res) => {
   res.render('index.ejs')
 });
 
-
-const MAX_ROOMS = 5
-
-var freerooms = [1,2,3,4,5]
-
-function joiningRoom(res, room) {
-  console.log("joining")
-  res.end(`Welcome to room - ${room}`)
-}
-
-function createRoom(res) {
-  console.log("creating")
-  let room = freerooms.pop()
-  // TODO handle case when we do not have room
-  res.redirect('http://localhost:3000/room/' + room);
-}
-
-function renderRoom(req, res) {
-  console.log("render")
-  let room = parseInt(req.params.id)
-  if (Number.isInteger(room)) {
-    if (room > 0 && room <= MAX_ROOMS) {
-      if (room in freerooms)
-        res.end(`Room not created - ${room}`)
-      else
-        joiningRoom(res, room)
-    }
-    else if (room == 0)
-      createRoom(res)
-  }
-}
-
 app.get("/room/:id",
  (req, res) => { 
-  renderRoom(req, res);});
+  rooms.renderRoom(req, res);});
 
 app.use((req,res,next) =>{res.render('404.ejs', { url :req.url});});
 
--- rooms.js
+++ rooms.js
@@ -3,24 +3,33 @@ const MAX_ROOMS = 5
 var freerooms = [1,2,3,4,5]
 
 function joiningRoom(res, room) {
+  console.log("joining")
   res.end(`Welcome to room - ${room}`)
 }
 
 function createRoom(res) {
+  console.log("creating")
   let room = freerooms.pop()
-  joiningRoom(res, room)
+  // TODO handle case when we do not have room
+  res.redirect('http://localhost:3000/room/' + room);
 }
 
 function renderRoom(req, res) {
-  if (Number.isInteger(req.params.id)) {
-    let room = req.params.id
-    if (room > 0 && room < MAX_ROOMS) {
-      if (room in freeroms)
+  console.log("render")
+  let room = parseInt(req.params.id)
+
+  if (Number.isInteger(room)) {
+    if (room > 0 && room <= MAX_ROOMS) {
+      if (room in freerooms)
         res.end(`Room not created - ${room}`)
       else
         joiningRoom(res, room)
     }
-    else if (req.params.id == 0)
+    else if (room == 0)
       createRoom(res)
   }
+}
+
+module.exports = {
+  renderRoom:renderRoom
 }

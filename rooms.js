2b94486fc12683c6bff98ad262b445e2e4c01b66
wiklam <wiktorpilar99@gmail.com>
2021-02-02 21:20:58 +0100

    Small fix

--- rooms.js
+++ rooms.js
@@ -1,13 +1,13 @@
 const MAX_ROOMS = 5
 
-var freerooms = [1,2,3,4,5]
-var roomusers = {} // user counter for each room
+var freerooms = [1,2,3,4,5,]
+var usersinroom = {} // user counter for each room
 
 
 function joiningRoom(res, room) {
   console.log("joining")
-  roomusers[room] +=1; // increment user counter
-  res.render('room.ejs', {id: room, users: roomusers[room]})
+  usersinroom[room] +=1; // increment user counter
+  res.render('room.ejs', {id: room, users: usersinroom[room]})
 }
 
 function createRoom(res) {
@@ -17,7 +17,7 @@ function createRoom(res) {
     // TODO write specific view for 'no rooms are currently available'
     res.render('no_rooms_available.ejs')
   } else{
-    roomusers[room]=0;
+    usersinroom[room]=0;
     res.redirect('http://localhost:3000/room/' + room);
   }
 }
@@ -28,7 +28,10 @@ function renderRoom(req, res) {
 
   if (Number.isInteger(room)) {
     if (room > 0 && room <= MAX_ROOMS) {
-      if (room in freerooms)
+      console.log(room)
+      console.log(freerooms)
+      console.log(room in freerooms)
+      if (freerooms.includes(room))
         res.end(`Room not created - ${room}`)
       else
         joiningRoom(res, room)

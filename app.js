798851aed38c1a32b5c5065cb52bb4dc68023261
Martyna Firgolska <mafirgolska@gmail.com>
2021-02-14 10:19:37 +0100

    Added username and username change

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var http = require('http');
 const path = require('path')
 var express = require('express');
 var socket = require('socket.io');
+var cookieParser= require('cookie-parser');
+
 
 var app = express();
 var rooms = require('./rooms')
@@ -13,16 +15,32 @@ app.use("/css", express.static(__dirname + "/views/css/"));
 app.use("/scripts", express.static(__dirname + "/views/scripts"));
 app.set('view engine', 'ejs');
 app.set('views', './views');
+app.disable('etag');
+app.use(cookieParser());
 app.use(
   express.urlencoded({
     extended: true
   })
 );
 
+function get_username(req, res){
+  if(!req.cookies.username) {
+    console.log('new player name')
+    username = 'anon'
+    res.cookie('username', username);
+  } 
+  else{
+    console.log('using old player name')
+    username = req.cookies.username;
+  }
+  return username
+}
+
 app.get('/', (req, res) => {
   // console.log("got get request for index");
   roomsTable = rooms.returnRooms();
-  res.render('index.ejs', {data: roomsTable});
+  username = get_username(req, res);
+  res.render('index.ejs', {data: roomsTable, username: username});
 });
 
 app.post( '/', (req, res) => {
@@ -38,11 +56,26 @@ app.post( '/', (req, res) => {
   }
 });
 
+app.get('/usernameChange', (req, res)=>{
+  username = get_username(req, res);
+  res.render('usernameChange.ejs', {username: username})
+})
+app.post('/usernameChange', (req, res)=>{
+  if(req.body.username){
+    res.cookie('username', req.body.username);
+    res.redirect('http://localhost:3000');
+  } else {
+    res.render('usernameChange.ejs', {
+      username: get_username(req, res),
+      message: 'You need to write your new username'});
+  }
+})
+
 server.listen(3000); 
 
 app.get("/room/:id", (req, res) => { 
   console.log("got get request for room", req.params.id)
-  res.render('tictac.ejs', {id: req.params.id});
+  res.render('tictac.ejs', {id: req.params.id, username: get_username(req, res)});
   // rooms.renderRoom(req, res);});
 });
 
